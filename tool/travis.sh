bfbc41bd16d2c6333ba9259c38e8fc35416a9d0e
Jorg Janke <jorg.janke@accorto.com>
2015-09-22 13:53:24 -0700

    travis setup

---
+++ tool/travis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Fast fail the script on failures.
+set -e
+
+# Verify that the libraries are error free.
+echo "Running dartanalyzer..."
+dartanalyzer --fatal-warnings \
+	lib/lightning.dart \
+	lib/lightning_ctrl.dart \
+	web/demo.dart \
+	test/lightning_test.dart
+
+# Run the tests.
+echo "Running tests..."
+pub run test
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  echo "Running coverage..."
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --retry 2 \
+    --exclude-test-files \
+    --debug \
+    test/lightning_all.dart
+fi
