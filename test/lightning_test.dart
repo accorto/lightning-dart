471a3f2bb14fe119259e761f0d1f7cbf38de0533
Jorg Janke <jorg.janke@accorto.com>
2015-12-01 08:59:54 -0800

    test fix (number of components)

--- test/lightning_test.dart
+++ test/lightning_test.dart
@@ -46,7 +46,7 @@ void main() {
     test('First Test', () {
       expect(page, isNotNull);
       expect(demoPage, isNotNull);
-      expect(demoPage.element.children.length, equals(32));
+      expect(demoPage.element.children.length, equals(31));
       print('first test');
     });
   }); // Components
